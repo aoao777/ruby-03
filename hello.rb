8d68351c56a39760d211cb5a15b4a41546e4fb52
Kudo Masaki <tabletennis.km0703@icloud.com>
2018-11-28 21:38:45 +0900

    ruby03

---
+++ hello.rb
@@ -0,0 +1,20 @@
+n = 3
+if n = 3
+  p "nは3です"
+end
+
+n = 4
+if  n = 3 || n = 4
+  p "3,4のどちらかです"
+end
+
+n = 4
+if  n = 3
+  p "nは３です"
+else n = 4
+  p "nは４です"
+end
+
+for massage in "こんにちわ" do
+  p message
+end
